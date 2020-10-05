168a75477b3d8ca27b52f96a086c1a88a87b36a6
ramcj7742 <71136833+ramcj7742@users.noreply.github.com>
2020-10-05 08:44:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Checks if user number is prime or not
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates scanner for user input
+    Scanner input = new Scanner (System.in);
+
+    // Gets user number
+    System.out.println("Which number would you like to check?");
+    int userNum = input.nextInt();
+
+    //creates variable to count up to userNum, check if number is divisable by the count num, and counts amount of times the number is divided
+    int count = 0;
+    int checkPrime = 0;
+    int divideCount = 0;
+
+    // creates loop that checks if number is prime
+    while(count < userNum){
+      count = count + 1;
+      //checks if number can divide by the current count num
+      checkPrime = userNum % count;
+      //adds to divide count if remainder is 0, sets division checker to 0
+      if(checkPrime == 0){
+        divideCount = divideCount + 1;
+        checkPrime = 0;
+      }else checkPrime = 0;
+    }
+    //displays to user the results based on how amny times the remainder is 0
+    if(divideCount > 2){
+      System.out.println(userNum + " is not a prime number");
+    }else if(divideCount <= 2){
+      System.out.println(userNum + " is a prime number");
+    }
   }
 }
